199767ce06d356cfbd826542181d00d4beb50dfc
Aaron Chan <aarchan108@gmail.com>
2026-03-18 17:53:14 -0400

    REname to marshal files and update values

--- boards/marshal/board.cmake
+++ boards/marshal/board.cmake
@@ -2,8 +2,8 @@
 
 # keep first
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
-board_runner_args(jlink "--device=STM32F030R8" "--speed=4000")
-board_runner_args(probe-rs "--chip=STM32F030R8Tx")
+board_runner_args(jlink "--device=STM32G474CC" "--speed=4000")
+board_runner_args(probe-rs "--chip=stm32g474cctx")
 
 # keep first
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
