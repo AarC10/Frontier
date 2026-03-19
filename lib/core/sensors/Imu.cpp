e4980bfbaf98dd0df754d63813fe72453a3d64fa
Aaron Chan <aarchan108@gmail.com>
2026-03-19 12:10:57 -0400

    Add subdir for cmake to sensors

--- lib/core/sensors/Imu.cpp
+++ lib/core/sensors/Imu.cpp
@@ -3,14 +3,14 @@
 #include <cmath>
 #include <zephyr/kernel.h>
 
-Imu::Imu(const device *dev) : dev_(dev) {}
+Imu::Imu(const device *dev) : dev(dev) {}
 
 int Imu::init(sensor_trigger_handler_t dataReadyHandler) {
-	if (dev_ == nullptr) {
+	if (dev == nullptr) {
 		return -EINVAL;
 	}
 
-	if (!device_is_ready(dev_)) {
+	if (!device_is_ready(dev)) {
 		return -ENODEV;
 	}
 
@@ -22,48 +22,48 @@ int Imu::init(sensor_trigger_handler_t dataReadyHandler) {
 	trigger.type = SENSOR_TRIG_DATA_READY;
 	trigger.chan = SENSOR_CHAN_ACCEL_XYZ;
 
-	lastError_ = sensor_trigger_set(dev_, &trigger, dataReadyHandler);
-	return lastError_;
+	lastError = sensor_trigger_set(dev, &trigger, dataReadyHandler);
+	return lastError;
 }
 
 ImuSample Imu::sample() {
-	if (dev_ == nullptr || !device_is_ready(dev_)) {
-		lastError_ = -ENODEV;
-		return lastSample_;
+	if (dev == nullptr || !device_is_ready(dev)) {
+		lastError = -ENODEV;
+		return lastSample;
 	}
 
-	lastError_ = sensor_sample_fetch(dev_);
-	if (lastError_ != 0) {
-		return lastSample_;
+	lastError = sensor_sample_fetch(dev);
+	if (lastError != 0) {
+		return lastSample;
 	}
 
 	sensor_value accel[3]{};
 	sensor_value gyro[3]{};
 
-	lastError_ = sensor_channel_get(dev_, SENSOR_CHAN_ACCEL_XYZ, accel);
-	if (lastError_ != 0) {
-		return lastSample_;
+	lastError = sensor_channel_get(dev, SENSOR_CHAN_ACCEL_XYZ, accel);
+	if (lastError != 0) {
+		return lastSample;
 	}
 
-	lastError_ = sensor_channel_get(dev_, SENSOR_CHAN_GYRO_XYZ, gyro);
-	if (lastError_ != 0) {
-		return lastSample_;
+	lastError = sensor_channel_get(dev, SENSOR_CHAN_GYRO_XYZ, gyro);
+	if (lastError != 0) {
+		return lastSample;
 	}
 
-	lastSample_.accelX = accel[0];
-	lastSample_.accelY = accel[1];
-	lastSample_.accelZ = accel[2];
-	lastSample_.gyroX = gyro[0];
-	lastSample_.gyroY = gyro[1];
-	lastSample_.gyroZ = gyro[2];
+	lastSample.accelX = accel[0];
+	lastSample.accelY = accel[1];
+	lastSample.accelZ = accel[2];
+	lastSample.gyroX = gyro[0];
+	lastSample.gyroY = gyro[1];
+	lastSample.gyroZ = gyro[2];
 
-	return lastSample_;
+	return lastSample;
 }
 
 float Imu::accelMagnitudeMg() const {
-	const double ax = sensor_value_to_double(&lastSample_.accelX);
-	const double ay = sensor_value_to_double(&lastSample_.accelY);
-	const double az = sensor_value_to_double(&lastSample_.accelZ);
+	const double ax = sensor_value_to_double(&lastSample.accelX);
+	const double ay = sensor_value_to_double(&lastSample.accelY);
+	const double az = sensor_value_to_double(&lastSample.accelZ);
 
 	return static_cast<float>(std::sqrt(ax * ax + ay * ay + az * az) * 1000.0);
 }
