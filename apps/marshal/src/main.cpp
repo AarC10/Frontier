0f5d64f09eb94b806cb9128f354dfb8458ca26b8
Aaron Chan <aarchan108@gmail.com>
2026-03-19 14:47:00 -0400

    Define flight records for logging

--- apps/marshal/src/main.cpp
+++ apps/marshal/src/main.cpp
@@ -14,6 +14,7 @@
 #include <core/sensors/Imu.h>
 #include <core/sensors/VoltageMonitor.h>
 #include <core/flight/FlightStateMachine.h>
+#include <core/flight_logger/FlightLogRecords.h>
 
 int main() {
     Barometer barometer(DEVICE_DT_GET(DT_ALIAS(barometer)));
