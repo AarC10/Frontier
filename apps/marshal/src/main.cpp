a153d3da14ef518630b76b7e9ec9e9d01c829030
Aaron Chan <aarchan108@gmail.com>
2026-03-28 01:40:11 -0400

    Working shell and log at the same time

--- apps/marshal/src/main.cpp
+++ apps/marshal/src/main.cpp
@@ -1,14 +1,7 @@
-/*
- * Copyright (c) 2026 Aaron Chan
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include <core/io/Led.h>
-#include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/logging/log_ctrl.h>
 
 LOG_MODULE_REGISTER(marshal, LOG_LEVEL_INF);
 
@@ -16,8 +9,10 @@ static gpio_dt_spec statusLedSpec = GPIO_DT_SPEC_GET(DT_ALIAS(led), gpios);
 static Led statusLed(&statusLedSpec);
 
 int main() {
-    statusLed.init();
+    int rc = statusLed.init();
+
     LOG_INF("Hello World!");
+
     while (true) {
         statusLed.toggle();
         k_msleep(1000);
